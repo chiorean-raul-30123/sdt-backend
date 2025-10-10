24f23b91d2c308ca962c5bf0a9022410d60102a4
Raul Chiorean <Raul-Gabriel.Chiorean@endava.com>
2025-10-10 18:51:59 +0300

    feat: add PackageDelivery API (create, assign, deliver, list by courier)

--- src/main/java/com/example/sdt/repo/PackageRepository.java
+++ src/main/java/com/example/sdt/repo/PackageRepository.java
@@ -5,8 +5,10 @@ import com.example.sdt.domain.PackageStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface PackageRepository extends JpaRepository<PackageDelivery, Long> {
     List<PackageDelivery> findByCourierId(Long courierId);
     List<PackageDelivery> findByStatus(PackageStatus status);
+    Optional<PackageDelivery> findByTrackingCode(String trackingCode);
 }
